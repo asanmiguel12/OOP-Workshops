3b427f930f5bbdb249040f61628673aa1b0d495d
Adrian <adrianchristiansanmiguel@gmail.com>
2024-10-24 23:31:54 -0700

    Initial Commit

---
+++ src/main/java/com/pluralsight/Card.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+
+public class Card {
+    private String suit;
+    private String value;
+    private boolean isFaceUp;
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+        this.isFaceUp = false;
+    }
+    public String getSuit(){
+    // only return the suit if the card is face up
+        if(isFaceUp){
+            return suit;
+        } else {
+            return  "#";
+        }
+    }
+    public String getValue(){
+    // only return the value if the card is face up
+        if(isFaceUp) {
+            return value;
+            } else {
+            return "#";
+        }
+    }
+    public int getPointValue() {
+    // only return the value if the card is face up
+        if(isFaceUp){
+            String[] faceCards = {"K", "Q", "J" };
+            for (String KThroughQ: faceCards) {
+                KThroughQ = "10";
+                int KThroughQValue = Integer.parseInt(KThroughQ);
+                return KThroughQValue;
+            }
+                String[] Ace = {"11","1"};
+                for (String A : faceCards) {
+                    int AceValue = Integer.parseInt(A);
+                    return AceValue;
+                }
+    // determine point value and return it A = 11 - K, Q, J = 10 - numbered cards are equal to their face value
+        } else {
+            String[] values = {"2","3","4","5","6","7","8","9","10"};
+            for(String numberValue: values){
+                int pointValue = Integer.parseInt(numberValue);
+                return pointValue;
+            }
+        }
+        return getPointValue();
+    }
+    public boolean isFaceUp(){
+        return isFaceUp;
+    }
+    public void flip(){
+        isFaceUp = !isFaceUp;
+    }
+}
---
+++ src/main/java/com/pluralsight/MainApp.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.util.Arrays;
+
+public class MainApp
+{
+    public static void main( String[] args ) {
+
+        Deck deck = new Deck();
+
+        Hand hand1 = new Hand();
+
+        deck.shuffle();
+        /* deal 5 cards */
+        for (int i = 0; i < 5; i++) {
+            /* get a card from the deck */
+            Card card = deck.deal();
+            /* deal that card to the hand */
+            hand1.deal(card);
+            deck.shuffle();
+            System.out.println(hand1);
+        }
+        int handValue = hand1.getValue();
+        System.out.println("This hand is worth " + handValue);
+    }
+}
+
+
